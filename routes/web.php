16c64263d3a8f72445ec5603b8390e6f89d63ce1
WimvdLinden <88454@mborijnland.nl>
2022-06-27 10:09:17 +0200

    paginas toegevoegd ook de inhoud van liam note: onthoud dat de paginas nog niet berijkbaar zijn via de index + dat de paginas nog ge edit moeten worden

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dist.index');
 });
 
 Route::get('/admin', function () {
@@ -26,3 +27,5 @@ Route::get('/admin/posts', function () {
 })->middleware(['auth'])->name('admin.posts');
 
 require __DIR__.'/auth.php';
+
+
