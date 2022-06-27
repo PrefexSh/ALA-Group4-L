e0d010eba3e2f3d606aadec3268118a407917480
PrefexSh <6018159@mborijnland.nl>
2022-06-27 10:45:24 +0200

    Move files to correct directories.

--- resources/views/dist/index.blade.php
+++ resources/views/dist/index.blade.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="shortcut icon" href="./img/fav.png" type="image/x-icon">
+  <link rel="shortcut icon" href="{{ asset('img/fav.svg') }}" type="image/x-icon">
   <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.12.1/css/pro.min.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
   <title>imkersshop</title>
 </head>
 <body class="bg-gray-100">
@@ -16,7 +16,7 @@
 
     <!-- logo -->
     <div class="flex-none w-56 flex flex-row items-center">
-      <img src="img/dollbij.gif" class="w-10 flex-none">
+      <img src="{{ asset('img/dollbij.gif') }}" class="w-10 flex-none">
       <strong class="capitalize ml-1 flex-1">imkershop</strong>
 
       <button id="sliderBtn" class="flex-none text-right text-gray-900 hidden md:block">
