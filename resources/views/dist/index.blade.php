e1b5a96b9b01ea05f002ed01c8c1ac1816545cb6
Wim <88454@mborijnland.nl>
2022-07-05 00:39:00 +0200

    navbar en header aangepast niet veel

--- resources/views/dist/index.blade.php
+++ resources/views/dist/index.blade.php
@@ -9,6 +9,8 @@
   <title>imkersshop</title>
 </head>
 <body class="bg-gray-100">
+big meaty dicks yum
+
 
 
 <!-- start navbar -->
@@ -322,101 +324,27 @@
       <p class="uppercase text-xs text-gray-600 mb-4 tracking-wider">homes</p>
 
       <!-- link -->
-      <a href="./index.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-chart-pie text-xs mr-2"></i>
-        Analytics dashboard
-      </a>
-      <!-- end link -->
-
-      <!-- link -->
-      <a href="./index-1.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-shopping-cart text-xs mr-2"></i>
-        ecommerce dashboard
-      </a>
-      <!-- end link -->
-
-      <p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">apps</p>
-
-      <!-- link -->
-      <a href="{{url('/cursussen')}}" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-graduation-cap text-xs mr-2"></i>
-          cursussen
-      </a>
-      <!-- end link -->
-
-      <!-- link -->
-      <a href="{{url('/cursussen')}}" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-plus text-xs mr-2"></i>
-        cursusblog
-      </a>
-      <!-- end link -->
-
-      <p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">UI Elements</p>
-
-      <!-- link -->
-      <a href="./typography.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-text text-xs mr-2"></i>
-        typography
-      </a>
-      <!-- end link -->
-
-      <!-- link -->
-      <a href="./alert.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-whistle text-xs mr-2"></i>
-        alerts
-      </a>
-      <!-- end link -->
-
-
-      <!-- link -->
-      <a href="./buttons.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-cricket text-xs mr-2"></i>
-        buttons
-      </a>
-      <!-- end link -->
-
-      <!-- link -->
-      <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-box-open text-xs mr-2"></i>
-        Content
-      </a>
-      <!-- end link -->
-
-      <!-- link -->
-      <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-swatchbook text-xs mr-2"></i>
-        colors
-      </a>
-      <!-- end link -->
-
-      <!-- link -->
-      <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-atom-alt text-xs mr-2"></i>
-        icons
-      </a>
-      <!-- end link -->
-
-      <!-- link -->
-      <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-club text-xs mr-2"></i>
-        card
-      </a>
-      <!-- end link -->
-
-      <!-- link -->
-      <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-cheese-swiss text-xs mr-2"></i>
-        Widgets
-      </a>
-      <!-- end link -->
-
-      <!-- link -->
-      <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-        <i class="fad fa-computer-classic text-xs mr-2"></i>
-        Components
-      </a>
-      <!-- end link -->
-
+        <a href="./index.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+            <img src="{{ asset('img/BeeExplorer.png    ') }}" class="w-10 fad text-xs mr-2">Cursusblog</i>
+        </a>
+        <a href="./index.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+            <img src="{{ asset('img/BeeExplorer.png    ') }}" class="w-10 fad text-xs mr-2">Cursusblog</i>
+        </a>
+        <a href="./index.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+            <img src="{{ asset('img/BeeExplorer.png    ') }}" class="w-10 fad text-xs mr-2">Cursusblog</i>
+        </a>
+        <a href="./index.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+            <img src="{{ asset('img/BeeExplorer.png    ') }}" class="w-10 fad text-xs mr-2">Cursusblog</i>
+        </a>
+        <a href="./index.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+            <img src="{{ asset('img/BeeExplorer.png    ') }}" class="w-10 fad text-xs mr-2">Cursusblog</i>
+        </a>
+        <a href="./index.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+            <img src="{{ asset('img/BeeExplorer.png    ') }}" class="w-10 fad text-xs mr-2">Cursusblog</i>
+        </a>
+        <a href="./index.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+            <img src="{{ asset('img/BeeExplorer.png    ') }}" class="w-10 fad text-xs mr-2">Cursusblog</i>
+        </a>
 
 
     </div>
