32dd3ec3a2cf9aa4c608ababa249820b6c6feaeb
Wim <88454@mborijnland.nl>
2022-07-06 08:57:26 +0200

    wat aan de index veranderd nu zijn de paginaas voor het oog netjes aleen het mist wat content dit moet opgevuld worden zo snel mogelijk

--- resources/views/dist/index.blade.php
+++ resources/views/dist/index.blade.php
@@ -94,7 +94,7 @@
 
             <!-- body -->
 
-            <!-- item -->
+              <!-- item -->
             <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
 
               <div class="px-3 py-2 rounded mr-3 bg-gray-100 border border-gray-300">
@@ -337,6 +337,8 @@
 
 
   </div>
+
+
     <!-- end sidebar content -->
 
   </div>
