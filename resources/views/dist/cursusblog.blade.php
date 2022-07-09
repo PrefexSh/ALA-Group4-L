1209f457da7989943460738760557fafdd20fb8d
Wim <88454@mborijnland.nl>
2022-07-09 11:04:24 +0200

    blog niet optijd af kunnen maken wel wat gedaan

--- resources/views/dist/cursusblog.blade.php
+++ resources/views/dist/cursusblog.blade.php
@@ -16,6 +16,8 @@
 
         /* Header/Blog Title */
         .header {
+
+            background-image:url({{url('img/beebolg.png')}});
             padding: 30px;
             font-size: 40px;
             text-align: center;
@@ -76,42 +78,47 @@
 </head>
 <body>
 
-<div class="header">
-    <h2>Blog Name</h2>
+<div  class="header">
+
+
+    <h2>Imkerblog</h2>
+
+
+
+
 </div>
 
 <div class="row">
     <div class="leftcolumn">
         <div class="card">
-            <h2>TITLE HEADING</h2>
+            <h2>imkerblog start</h2>
             <h5>Title description, Dec 7, 2017</h5>
             <div class="fakeimg" style="height:200px;">Image</div>
-            <p>Some text..</p>
+            <p>lorum ipsom</p>
             <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
         </div>
         <div class="card">
-            <h2>TITLE HEADING</h2>
+            <h2>custom bijennest</h2>
             <h5>Title description, Sep 2, 2017</h5>
             <div class="fakeimg" style="height:200px;">Image</div>
-            <p>Some text..</p>
+            <p>lorum ipsom</p>
             <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
         </div>
     </div>
     <div class="rightcolumn">
         <div class="card">
-            <h2>About Me</h2>
+            <h2>over mij</h2>
             <div class="fakeimg" style="height:100px;">Image</div>
-            <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+            <p> lorum ipsom in culpa qui officia deserunt mollit anim..</p>
         </div>
         <div class="card">
-            <h3>Popular Post</h3>
-            <div class="fakeimg">Image</div><br>
-            <div class="fakeimg">Image</div><br>
-            <div class="fakeimg">Image</div>
+            <h3>populaire posts</h3>
+
+            <img src="{{ asset('img/beebolg.png') }}"width="180" height="250" >
         </div>
         <div class="card">
             <h3>Follow Me</h3>
-            <p>Some text..</p>
+            <p>lorum ipsom</p>
         </div>
     </div>
 </div>
